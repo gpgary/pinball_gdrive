fb74aedbfe88baf2b5ebdcb4ed9fc4b84f9217ab
gary.lin <gary.lin@atgamescloud.net>
2020-06-23 17:09:38 +0800

    commit

---
+++ main.py
@@ -0,0 +1,59 @@
+
+from __future__ import print_function
+import httplib2
+import os, io
+
+from apiclient import discovery
+from oauth2client import client
+from oauth2client import tools
+from oauth2client.file import Storage
+from apiclient.http import MediaFileUpload, MediaIoBaseDownload
+try:
+    import argparse
+    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+except ImportError:
+    flags = None
+import auth
+# If modifying these scopes, delete your previously saved credentials
+# at ~/.credentials/drive-python-quickstart.json
+SCOPES = 'https://www.googleapis.com/auth/drive'
+CLIENT_SECRET_FILE = 'C:\\GDrive_download\\client_secret.json'
+APPLICATION_NAME = 'Drive API Python Quickstart'
+authInst = auth.auth(SCOPES,CLIENT_SECRET_FILE,APPLICATION_NAME)
+credentials = authInst.getCredentials()
+
+http = credentials.authorize(httplib2.Http())
+drive_service = discovery.build('drive', 'v3', http=http)
+
+def downloadFile(file_id,filepath):
+    print('download target '+file_id+' filepath '+filepath)
+    request = drive_service.files().get_media(fileId=file_id)    
+    fh = io.BytesIO()
+    downloader = MediaIoBaseDownload(fh, request)
+    done = False
+    while done is False:
+        status, done = downloader.next_chunk()
+        print("Download %d%%." % int(status.progress() * 100))
+    with io.open(filepath,'wb') as f:
+        fh.seek(0)
+        f.write(fh.read())
+        
+    exit()
+
+def searchFile(size,query):
+    name = ''
+    fieldId = ''
+    results = drive_service.files().list(
+    pageSize=size,fields="nextPageToken, files(id, name, kind, mimeType)",q=query).execute()
+    items = results.get('files', [])    
+    if not items:
+        print('No files found.')
+    else:
+        print('Files:')
+        for item in items:
+            print(item)
+            print('{0} ({1})'.format(item['name'], item['id']))
+            downloadFile(item['id'],'C:\\GDrive_download\\download\\'+item['name'])
+            print('End downlaod '+item['name']+' !!')
+    
+searchFile(10,"name contains 'pUCE'")
